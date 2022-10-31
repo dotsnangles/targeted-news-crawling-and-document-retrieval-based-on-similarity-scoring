0eabce1184cb8a9ff26376451cd7b637267fd060
Jeonghyeon Park <dotsnangles@gmail.com>
2022-10-31 13:34:04 +0900

    UPDATE: filter added and v2 complete

--- main.py
+++ main.py
@@ -1,63 +1,49 @@
-import time
-import re
+import json
 import argparse
-import requests
 import pandas as pd
-from bs4 import BeautifulSoup
-from module.crawler import crawl_news_list
+from module.crawler import get_news_list, crawl_news
 
-### 실행 방법
-### python main.py --query "키워드"
+### python main.py --query '키워드' --org '기관명'
 
 def main():
-    # 터미널 실행시 argument를 받기 위한 코드입니다.
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--query", dest="query", action="store")
-    args = parser.parse_args()
+    client_id = 'hwEnzlSdGXZTyeubesmy'
+    client_secret = 'WkpCvqbw6u'
 
-    # 네이버 API에서 키워드로 검색된 뉴스의 목록을 불러옵니다.
-    news_df = crawl_news_list(args.query)
-
-    # 이제부터 크롤링할 본문을 저장해둘 리스트입니다.
-    articles = []
-
-    # 접근 권한을 획득하기 위해 브라우저의 user-agent와 cookie를 가지고 왔습니다.
-    # 크롤러가 작동하지 않을 경우 cookie를 변경해주면 다시 작동합니다.
     headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
         'cookie': 'nid_inf=1417715440; NID_AUT=jB6JKkl0bQJkzabA2sqcsZPcc58lN3Nk/oKjGyVYl1n8PedB7ujlxHdECKcm9kS7; NID_JKL=CdIfrElqYRkes7uT/S10prytLgVd7C1HBLS0nz55Sys=; VISIT_LOG_CLEAN=1; NFS=2; NID_SES=AAABkxQsSdlab5VkBVUMJHd12mikXaT/HRo/rtzzaxgjXmXJLfOFG25S5TiaZsFNk5bLeRF4Ej3UoZTevQC7Jd7fVFOAFJtSFoNgapi8qqUTVHDqzpWo4zUjmx/LD5cdsWbvaL/cByCai8XYLRLdB0o6W7sQutPmRMFUblZMKU/CdpBh8aziLA/i9wHRgkCr4zpYxc/YROLyZLZ7QmphI5qakKYHN4OxNKYy/Dp+PvlCsWk6azoXw7AxABwR/NdLMwDxNgZ6yN0iUJDkWFFPEN0yBGCm5VaOa1oF4tU0GkLMQsbyMb/+mOw6aBjxQNSgS5RDsn3Vs6H9l1qczNtasU4OpdonIK0EzoLYyQEgLkpXeLTnzkdaJnqR7EyjKjl8hH/EsaVdnRaAEs0afY3VK8f+pCn0z/5n9xpiCd0UVrpoI3nwF1No4x/mEk71EW2ghH90hyg/9F0KHAHLneYZ6lCvcdhE/ENHrmc1nP0R0sUnOltV0XIyf0fEua7rvSqv0P0iuE0xD2+WUqh//96WFUP+XyeQxvn16XPBT8PSfE/nU/FQ; nx_ssl=2; page_uid=h1WRNsp0YiRssKvZbrNssssssMh-389234; BMR='
     }
 
-    # 본문 크롤링을 시작합니다. 네이버 뉴스 연예의 경우 html tag의 class와 id가 다른 카테고리와는 다릅니다.
-    # 따라서 예외처리를 해줍니다.
-    # 네이버뉴스 일반: #newsct_article 연예: #articeBody
-    # 뉴스 항목의 리스트(news_df)를 반복문에 넣어줍니다.
-    for idx, row in news_df.iterrows():
-        # row.title, row.originallink, row.link, row.description, row.pubDate,
-        URL = str(row.link)
-        r = requests.get(URL, headers=headers)
-        
-        # 연예 카테고리의 태그를 잡지 못 해 생기는 에러를 회피하기 위한 코드가 except로 추가되어 있습니다.
-        try:
-            soup = BeautifulSoup(r.content, 'html5lib')
-            article = soup.select('#newsct_article')[0].text
-            cleansed_article = re.sub(r'[\n\t^/$]', '', article)
-            articles.append([row.pubDate, row.title, cleansed_article, row.originallink, row.link, row.description,])
-        except Exception as e:
-            soup = BeautifulSoup(r.content, 'html5lib')
-            article = soup.select('#articeBody')[0].text
-            cleansed_article = re.sub(r'[\n\t^/$]', '', article)
-            articles.append([row.pubDate, row.title, cleansed_article, row.originallink, row.link, row.description,])
-        
-        # 반복적인 크롤링으로 인해 ban 당하는 것을 예방하기 위해 시간 지연 명령을 넣었습니다.
-        time.sleep(5)
-        
-        # 테스트를 위해 열개만 추출한 뒤 멈추는 코드입니다.
-        # if idx == 10:
-        #     break
+    with open('government_organization_chart.json', encoding='utf-8') as f:
+        gov_orgs = json.load(f)
 
-    result = pd.DataFrame(articles, columns=['pubDate', 'title', 'body', 'originallink', 'link', 'description'])
-    result.to_csv(f'{args.query}_crawled.csv', index=False)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--query", dest="query", action="store")
+    parser.add_argument("--org", dest="org", action="store")
+    args = parser.parse_args()
+
+    query = args.query
+    org_name = args.org
+    sub_orgs = gov_orgs[args.org]
+
+    crawling_trgs = []
+    for sub_org in sub_orgs:
+        crawling_trg = ' '.join([org_name, sub_org, query])
+        crawling_trgs.append(crawling_trg)
+
+    crawled_news_dfs = []
+    for crawling_trg in crawling_trgs:
+        news_list_df = get_news_list(crawling_trg, client_id, client_secret)
+        crawled_news_df = crawl_news(news_list_df, headers)
+        crawled_news_dfs.append(crawled_news_df)
+        
+    result = pd.concat(crawled_news_dfs)
+
+    search_sub_org = result.body.str.contains('|'.join(sub_orgs), case=False, regex=True)
+    search_query = result.body.str.contains(query, case=False, regex=True)
+    result = result[search_sub_org & search_query]
+
+    result.to_csv(f'{"_".join([query, org_name])}_crawled.csv', index=False)
 
 if __name__ == '__main__':
     main()
--- module/crawler.py
+++ module/crawler.py
@@ -4,48 +4,31 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import re
+import time
 
-### 네이버 developers에서 할당받은 id와 key입니다. 서빙시에는 환경 변수로 넣어두는 것이 좋을 것 같습니다.
+### Naver API에서 뉴스 항목을 불러옵니다.
 
-client_id = 'hwEnzlSdGXZTyeubesmy'
-client_secret = 'WkpCvqbw6u'
-
-### https://developers.naver.com/docs/serviceapi/search/news/news.md#뉴스-검색-결과-조회
-### 위 페이지를 참조하여 만든 뉴스 검색 결과를 조회해 가지고 오는 크롤러입니다.
-### JSON 포맷으로 'pubDate', 'title', 'originallink', 'link', 'description' 다섯가지 항목을 불러옵니다.
-### 쿼리 요청시에 파라미터로 정렬이 가능합니다. 관련도와 날짜 최신순으로 가능하며 현재 날짜 최신순으로 설정되어 있습니다.
-### 불러올 수 있는 항목의 최대 수는 1100개입니다.
-### 아래의 함수로 불러온 리스트를 바탕으로 main.py에서 본문 크롤링이 진행됩니다.
-
-def crawl_news_list(query):
+def get_news_list(crawling_trg, client_id, client_secret):
     news_list = []
-    
-    # 항목을 100개씩 불러오며 마지막 인덱스를 네이버 API에서 허용하는 최대값인 1000으로 바꿔줍니다.
-    # 인덱스의 최소값은 1로 허용되어 있습니다.
     for step in range(1, 1100, 100):
         if step == 1001:
             step = 1000
         
-        # 네이버 API의 파라미터를 설정하기 위한 코드입니다.
         params = dict(
-            query=urllib.parse.quote(query),
-            display=urllib.parse.quote('100'), # 항목을 100개씩 불러오기 위한 설정입니다. 최대 허용 수치는 100입니다.
+            query=urllib.parse.quote(crawling_trg),
+            display=urllib.parse.quote('100'), 
             start=urllib.parse.quote(str(step)),
             sort=urllib.parse.quote('date'),
         )
 
-        # 설정한 파라미터를 리퀘스트를 수행할 url에 넣습니다.
         url = f"https://openapi.naver.com/v1/search/news?query={params['query']}&display={params['display']}&start={params['start']}&sort={params['sort']}"
 
-        # 네이버 developers에서 할당받은 id와 key를 넣습니다.
         request = urllib.request.Request(url)
         request.add_header('X-Naver-Client-Id',client_id)
         request.add_header('X-Naver-Client-Secret',client_secret)
         response = urllib.request.urlopen(request)
         rescode = response.getcode()
 
-        # 응답 코드가 200이 나오면 결과를 가지고 옵니다. 그렇지 않은 경우 에러 메세지를 표시합니다.
-        # 최상단에 선언한 news_list에 하나씩 붙여넣기를 수행합니다.
         if(rescode==200):
             response_body = response.read()
             result = json.loads(response_body.decode('utf-8'))['items']
@@ -53,14 +36,44 @@ def crawl_news_list(query):
         else:
             print('Error Code:' + rescode)
         # print(step)
+        
+        time.sleep(5)
+        
+        if step == 101:
+            break
     
-    # 편리한 후처리를 위해 판다스의 데이터 프레임으로 변환합니다.
-    # 중복되는 행을 제거해줍니다.
-    # 또한 네이버 뉴스에 등록되지 않은 뉴스는 제외합니다.
-    news_df = pd.DataFrame(news_list).drop_duplicates()
-    news_df['checker'] = news_df.link.str.find('https://n.news.naver.com/')
-    news_df = news_df[news_df['checker'] > -1]
-    news_df.reset_index(inplace=True)
+    news_list_df = pd.DataFrame(news_list).drop_duplicates()
+    news_list_df['checker'] = news_list_df.link.str.find('https://n.news.naver.com/')
+    news_list_df = news_list_df[news_list_df['checker'] > -1]
+    news_list_df.reset_index(inplace=True)
+    news_list_df['crawling_trg'] = crawling_trg
     
-    # 데이터 프레임 변환 작업이 끝나면 반환해줍니다.
-    return news_df
+    return news_list_df
+
+### Naver API에서 불러온 뉴스 항목의 본문을 불러옵니다.
+
+def crawl_news(news_list_df, headers):
+    crawled_news = []
+    for idx, row in news_list_df.iterrows():
+        # row.crawling_trg, row.pubDate, row.title, cleansed_news_body, row.originallink, row.link, row.description
+        URL = str(row.link)
+        r = requests.get(URL, headers=headers)
+        
+        try:
+            soup = BeautifulSoup(r.content, 'html5lib')
+            news_body = soup.select('#newsct_article')[0].text
+            cleansed_news_body = re.sub(r'[\n\t^/$]', '', news_body)
+            crawled_news.append([row.crawling_trg, row.pubDate, row.title, cleansed_news_body, row.originallink, row.link, row.description])
+        except Exception as e:
+            soup = BeautifulSoup(r.content, 'html5lib')
+            news_body = soup.select('#articeBody')[0].text
+            cleansed_news_body = re.sub(r'[\n\t^/$]', '', news_body)
+            crawled_news.append([row.crawling_trg, row.pubDate, row.title, cleansed_news_body, row.originallink, row.link, row.description])
+        
+        time.sleep(5)
+        
+        if idx == 3:
+            break
+    
+    crawled_news_df = pd.DataFrame(crawled_news, columns=['crawling_trg', 'pubDate', 'title', 'body', 'originallink', 'link', 'description'])
+    return crawled_news_df
